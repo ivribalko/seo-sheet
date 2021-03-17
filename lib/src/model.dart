d68fc9cb95b8fc7ab169e9df181391a5cb8d3ae0
Ivan Rybalko <ivribalko@gmail.com>
2021-03-17 00:59:07 -0400

    test

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:get/get.dart';
+import 'package:google_place/google_place.dart';
 import 'package:gsheets/gsheets.dart';
 
 import 'common.dart';
@@ -10,11 +11,17 @@ class Model extends GetxController with Log {
   @override
   void onInit() async {
     super.onInit();
+
     await GSheets(await rootBundle.loadString('assets/account-key.json'))
         .spreadsheet('1_9FqFUwSYWLhLF-VEXJYMgD2CB8z0gKbP8BC8nHXa4Y')
         .then((value) => value.worksheetByTitle('Sheet1'))
         .then((value) async => await value?.values.column(1))
         .then((value) => urls.addAll(value!));
+
+    await GooglePlace(await rootBundle.loadString('assets/maps-key.txt'))
+        .details
+        .get('ChIJN1t_tDeuEmsRUsoyG83frY4', fields: 'user_ratings_total')
+        .then((value) => print(value!.result!.userRatingsTotal));
   }
 
   @override
