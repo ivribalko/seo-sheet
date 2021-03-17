9c8f68af934c8e864c78ce68a6285b64f73c2648
Ivan Rybalko <ivribalko@gmail.com>
2021-03-17 01:07:48 -0400

    get

--- lib/ioc.dart
+++ lib/ioc.dart
@@ -1,18 +1,22 @@
 import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:get/get.dart';
+import 'package:google_place/google_place.dart';
+import 'package:gsheets/gsheets.dart';
 import 'package:logging/logging.dart';
 
 import 'src/model.dart';
 
 class IoC {
-  static Future init() {
+  static Future init() async {
     _initLog();
 
+    Get.put(GooglePlace(await rootBundle.loadString('assets/maps-key.txt')));
+    Get.put(GSheets(await rootBundle.loadString('assets/account-key.json')));
     Get.put(Model());
-
-    return Future.value();
   }
 
   static void _initLog() {
--- lib/src/model.dart
+++ lib/src/model.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:get/get.dart';
 import 'package:google_place/google_place.dart';
 import 'package:gsheets/gsheets.dart';
@@ -7,19 +6,19 @@ import 'common.dart';
 
 class Model extends GetxController with Log {
   final urls = <String>[].obs;
+  final _GSheets = Get.find<GSheets>();
+  final _GooglePlace = Get.find<GooglePlace>();
 
   @override
   void onInit() async {
     super.onInit();
 
-    await GSheets(await rootBundle.loadString('assets/account-key.json'))
-        .spreadsheet('1_9FqFUwSYWLhLF-VEXJYMgD2CB8z0gKbP8BC8nHXa4Y')
+    await _GSheets.spreadsheet('1_9FqFUwSYWLhLF-VEXJYMgD2CB8z0gKbP8BC8nHXa4Y')
         .then((value) => value.worksheetByTitle('Sheet1'))
         .then((value) async => await value?.values.column(1))
         .then((value) => urls.addAll(value!));
 
-    await GooglePlace(await rootBundle.loadString('assets/maps-key.txt'))
-        .details
+    await _GooglePlace.details
         .get('ChIJN1t_tDeuEmsRUsoyG83frY4', fields: 'user_ratings_total')
         .then((value) => print(value!.result!.userRatingsTotal));
   }
