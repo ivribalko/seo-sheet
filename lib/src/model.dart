dace637c900a5a1de5b2a45dc3b9e3f676f2ba57
Ivan Rybalko <ivribalko@gmail.com>
2021-03-16 22:41:52 -0400

    no work

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -1,19 +1,36 @@
 import 'package:get/get.dart';
+import 'package:gsheets/gsheets.dart';
 
 import 'common.dart';
 
 class Model extends GetxController with Log {
-  final count = 0.obs;
+  final _credentials = r'''
+{
+  "type": "service_account",
+  "project_id": "",
+  "private_key_id": "",
+  "private_key": "",
+  "client_email": "",
+  "client_id": "",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": ""
+}
+''';
 
   @override
-  void onInit() {
+  void onInit() async {
     super.onInit();
-    count.listen(log.info);
+    final sheets = GSheets(_credentials);
+    final spreadsheet = await sheets
+        .spreadsheet('1_9FqFUwSYWLhLF-VEXJYMgD2CB8z0gKbP8BC8nHXa4Y');
+    var sheet = await spreadsheet.worksheetByTitle('Sheet1');
+    log.info(await sheet?.values.value(column: 1, row: 1));
   }
 
   @override
   void onClose() {
-    count.close();
     super.onClose();
   }
 }
