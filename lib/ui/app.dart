bb3bbd6c4e4624bb83871c162734ba716d3487c1
Ivan Rybalko <ivribalko@gmail.com>
2021-03-17 01:08:38 -0400

    rm page

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'home/home_page.dart';
-import 'page/page_page.dart';
 import 'routes.dart';
 
 class App extends StatelessWidget {
@@ -12,7 +11,6 @@ class App extends StatelessWidget {
       title: 'Flutter Demo',
       getPages: [
         GetPage(name: Routes.home, page: () => HomePage()),
-        GetPage(name: Routes.page, page: () => PagePage()),
       ],
     );
   }
