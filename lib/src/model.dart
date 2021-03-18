23fa984719fa2312c1668c919b171bc897ae56b3
Ivan Rybalko <ivribalko@gmail.com>
2021-03-18 00:25:22 -0400

    show multiple

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
 import 'package:google_place/google_place.dart';
 import 'package:gsheets/gsheets.dart';
@@ -14,26 +16,38 @@ class Model extends GetxController with Log {
     super.onInit();
     await _sheet
         .spreadsheet('1_9FqFUwSYWLhLF-VEXJYMgD2CB8z0gKbP8BC8nHXa4Y')
-        // get sheet
         .then((value) => value.worksheetByTitle('Sheet1'))
-        // get places urls
         .then((value) async => await value?.values.column(1))
-        .then((value) => value![1])
-        // get place name
-        .then((value) => Uri.parse(value).pathSegments[2])
-        // get place id
-        .then((value) async => await _place.search.getFindPlace(
-              value,
-              InputType.TextQuery,
-            ))
-        .then((value) {
-          assert(value!.candidates!.length == 1);
-          return value!.candidates![0].placeId;
-        })
-        .then((value) async => await _place.details.get(
-              value!,
-              fields: 'user_ratings_total',
-            ))
-        .then((value) => data.add(value!.result!.userRatingsTotal.toString()));
+        .then((value) => value!.map((e) => Future.value(e)
+            .then((value) => Uri.parse(value).pathSegments[2])
+            .then(getPlaces)
+            .then(toPlaceId)
+            .then(toDetails)))
+        .then((value) async => await Future.wait(value))
+        .then(toData)
+        .then(data.addAll);
+  }
+
+  FutureOr<DetailsResponse?> toDetails(String value) async {
+    return await _place.details.get(
+      value,
+      fields: 'user_ratings_total',
+    );
+  }
+
+  FutureOr<Iterable<String>> toData(Iterable<DetailsResponse?> value) {
+    return value.map((e) => e!.result!.userRatingsTotal.toString());
+  }
+
+  FutureOr<String> toPlaceId(FindPlaceResponse? value) {
+    // assert(value!.candidates!.length == 1);
+    return value!.candidates![0].placeId!;
+  }
+
+  FutureOr<FindPlaceResponse?> getPlaces(String value) async {
+    return await _place.search.getFindPlace(
+      value,
+      InputType.TextQuery,
+    );
   }
 }
