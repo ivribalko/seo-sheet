8b4f86cb4e93c647a7a703bb6c81398df8be92c5
Ivan Rybalko <ivribalko@gmail.com>
2021-03-21 14:00:01 -0400

    dupe page

---
+++ lib/ui/dupe/dupe_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:seosheet/src/model.dart';
+import 'package:seosheet/ui/common.dart';
+
+class DupePage extends StatelessWidget {
+  final model = Get.find<Model>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: CommonFutureBuilder<List<Verified>>(
+          future: model.data(Get.arguments[0], Get.arguments[1]),
+          result: (result) {
+            return Scrollbar(
+              child: ListView(
+                children: result!.asMap().entries.map(toTile).toList(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget toTile(MapEntry<int, Verified> data) {
+    return ListTile(
+      title: Text(data.value.text),
+      leading: Text('#${data.key + 1}'),
+      tileColor: data.value.failed ? Colors.yellow : Colors.transparent,
+    ).paddingAll(10);
+  }
+}
